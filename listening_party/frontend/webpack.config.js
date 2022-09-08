8a2750814e3500af20aac1761b584f7175d80088
Josh <joshh2201@gmail.com>
2022-09-08 11:31:28 -0400

    Setup Babel and Webpack configs

---
+++ listening_party/frontend/webpack.config.js
@@ -0,0 +1,36 @@
+// webpack bundles all JavaScript into one file and serves it to the browser
+const path = require("path");
+const webpack = require("webpack");
+
+module.exports = {
+    // define where to output JavaScript
+    entry: "./src/index.js",
+    output: {
+        path: path.resolve(__dirname, "./static/frontend"),
+        filename: "[name].js",
+    },
+    // exclude the node_modules folder in the bundle and use babel-loader
+    module: {
+        rules: [
+        {
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: {
+            loader: "babel-loader",
+            },
+        },
+        ],
+    },
+    // minimize unnecessary characters and whitespace in JavaScript
+    optimization: {
+        minimize: true,
+    },
+    plugins: [
+        new webpack.DefinePlugin({
+        "process.env": {
+            // This has effect on the react lib size
+            NODE_ENV: JSON.stringify("production"),
+        },
+        }),
+    ],
+};
