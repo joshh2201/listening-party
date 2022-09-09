794fa1b0a2c0d82af650cb6d98f755fd2e974800
Josh <joshh2201@gmail.com>
2022-09-08 23:21:20 -0400

    Added react components

---
+++ listening_party/frontend/src/components/CreateRoomPage.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class CreateRoomPage extends Component {
+  constructor(props) {
+    super(props); // calls constructor of parent
+  }
+
+  render() {
+    return <p>This is the create room page</p>;
+  }
+}
---
+++ listening_party/frontend/src/components/RoomJoinPage.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class RoomJoinPage extends Component {
+  constructor(props) {
+    super(props); // calls constructor of parent
+  }
+
+  render() {
+    return <p>This is the room join page</p>;
+  }
+}
