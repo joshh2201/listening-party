b34f0715c3538245fd16565be279e2d29363b93e
Josh <joshh2201@gmail.com>
2022-09-10 15:59:57 -0400

    Added event handling for create room page

--- listening_party/frontend/src/components/CreateRoomPage.js
+++ listening_party/frontend/src/components/CreateRoomPage.js
@@ -15,8 +15,44 @@ export default class CreateRoomPage extends Component {
   defaultVotes = 2;
   constructor(props) {
     super(props); // calls constructor of parent
+    this.state = {
+      guest_can_pause: true,
+      votes_to_skip: this.defaultVotes,
+    };
+
+    // binding gives access to "this" keyword for methods called in the DOM
+    this.handleRoomButtonPress = this.handleRoomButtonPress.bind(this);
+    this.handleVotesChange = this.handleVotesChange.bind(this);
+    this.handlePauseChange = this.handlePauseChange.bind(this);
   }
 
+  handleVotesChange(e) {
+    this.setState({
+      votes_to_skip: e.target.value,
+    });
+  }
+
+  handlePauseChange(e) {
+    this.setState({
+      guest_can_pause: e.target.value === 'true' ? true : false,
+    });
+  }
+
+  handleRoomButtonPress() {
+    const requestOptions = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        votes_to_skip: this.state.votes_to_skip,
+        guest_can_pause: this.state.guest_can_pause,
+      }),
+    };
+    // send request to /api/create endpoint with requestOptions payload
+    // after response is received, convert to json
+    fetch('/api/create', requestOptions)
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+  }
   render() {
     return (
       <Grid container spacing={1}>
@@ -27,10 +63,14 @@ export default class CreateRoomPage extends Component {
         </Grid>
         <Grid item xs={12} align='center'>
           <FormControl component='fieldset'>
-            <FormHelperText>
+            <FormHelperText component='div'>
               <div align='center'>Guest Control of Playback State</div>
             </FormHelperText>
-            <RadioGroup row defaultValue='true'>
+            <RadioGroup
+              row
+              defaultValue='true'
+              onChange={this.handlePauseChange}
+            >
               <FormControlLabel
                 value='true'
                 control={<Radio color='primary' />}
@@ -53,14 +93,19 @@ export default class CreateRoomPage extends Component {
               type='number'
               defaultValue={this.defaultVotes}
               inputProps={{ min: 1, style: { textAlign: 'center' } }}
+              onChange={this.handleVotesChange}
             />
-            <FormHelperText>
+            <FormHelperText component='div'>
               <div align='center'>Votes Required to Skip Song</div>
             </FormHelperText>
           </FormControl>
         </Grid>
         <Grid item xs={12} align='center'>
-          <Button color='primary' variant='contained'>
+          <Button
+            color='primary'
+            variant='contained'
+            onClick={this.handleRoomButtonPress}
+          >
             Create A Room
           </Button>
         </Grid>
