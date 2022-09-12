38caa857a229a631de271bed94d50d75862c0346
Josh <joshh2201@gmail.com>
2022-09-12 15:38:26 -0400

    Moved the router to the homepage

--- listening_party/frontend/src/components/App.js
+++ listening_party/frontend/src/components/App.js
@@ -1,16 +1,7 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link,
-  Redirect,
-} from 'react-router-dom';
+
 import HomePage from './Homepage';
-import RoomJoinPage from './RoomJoinPage';
-import CreateRoomPage from './CreateRoomPage';
-import Room from './Room';
 
 export default class App extends Component {
   constructor(props) {
@@ -20,15 +11,7 @@ export default class App extends Component {
   render() {
     return (
       <div className='center'>
-        <Router>
-          <Switch>
-            <Route exact path='/' component={HomePage} />
-            <Route path='/join' component={RoomJoinPage} />
-            <Route path='/create' component={CreateRoomPage} />
-            {/* colon denotes a url parameter */}
-            <Route path='/room/:roomCode' component={Room} />
-          </Switch>
-        </Router>
+        <HomePage />
       </div>
     );
   }
