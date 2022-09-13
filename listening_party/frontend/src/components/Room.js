bbaf91c9a58b4ac88461e572e81eeb6ec5b54e5b
Josh <joshh2201@gmail.com>
2022-09-12 20:47:51 -0400

    Added styling to the room page and routing for leaving  a room

--- listening_party/frontend/src/components/Room.js
+++ listening_party/frontend/src/components/Room.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { Grid, Button, Typography } from '@material-ui/core';
+import { Link } from 'react-router-dom';
 
 export default class Room extends Component {
   constructor(props) {
@@ -10,11 +12,29 @@ export default class Room extends Component {
     };
     this.roomCode = this.props.match.params.roomCode;
     this.getRoomDetails();
+    this.leaveButtonPressed = this.leaveButtonPressed.bind(this);
+  }
+
+  leaveButtonPressed() {
+    const requestOptions = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+    };
+    fetch('/api/leave-room', requestOptions).then((response) => {
+      this.props.leaveRoomCallback();
+      this.props.history.push('/');
+    });
   }
 
   getRoomDetails() {
     fetch('/api/get-room?code=' + this.roomCode)
-      .then((response) => response.json())
+      .then((response) => {
+        if (!response.ok) {
+          this.props.leaveRoomCallback();
+          this.props.history.push('/');
+        }
+        return response.json();
+      })
       .then((data) => {
         this.setState({
           votes_to_skip: data.votes_to_skip,
@@ -25,12 +45,37 @@ export default class Room extends Component {
   }
   render() {
     return (
-      <div>
-        <h3>{this.roomCode}</h3>
-        <p>votes {this.state.votes_to_skip}</p>
-        <p>guest can pause: {this.state.guest_can_pause.toString()}</p>
-        <p>host: {this.state.is_host.toString()}</p>
-      </div>
+      <Grid container spacing={1} align='center'>
+        <Grid item xs={12}>
+          <Typography variant='h4' component='h4'>
+            Code : {this.roomCode}
+          </Typography>
+        </Grid>
+        <Grid item xs={12}>
+          <Typography variant='h6' component='h6'>
+            Votes to Skip Song : {this.state.votes_to_skip}
+          </Typography>
+        </Grid>
+        <Grid item xs={12}>
+          <Typography variant='h6' component='h6'>
+            Guests can Pause : {this.state.guest_can_pause.toString()}
+          </Typography>
+        </Grid>
+        <Grid item xs={12}>
+          <Typography variant='h6' component='h6'>
+            Host : {this.state.is_host.toString()}
+          </Typography>
+        </Grid>
+        <Grid item xs={12}>
+          <Button
+            color='secondary'
+            variant='contained'
+            onClick={this.leaveButtonPressed}
+          >
+            Leave Room
+          </Button>
+        </Grid>
+      </Grid>
     );
   }
 }
