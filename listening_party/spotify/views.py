e9adc627404ef0de4caa57e7192367481fa236e9
Josh <joshh2201@gmail.com>
2022-09-18 23:58:50 -0400

    Added current song endpoint

--- listening_party/spotify/views.py
+++ listening_party/spotify/views.py
@@ -4,7 +4,8 @@ from rest_framework.views import APIView
 import requests
 from rest_framework import status
 from rest_framework.response import Response
-from .util import update_or_create_tokens, is_spotify_authenticated
+from .util import *
+from api.models import Room
 
 # Create your views here.
 class AuthURL(APIView):
@@ -23,8 +24,8 @@ class AuthURL(APIView):
 def spotify_callback(request, format=None):
     code = request.GET.get('code')
     error = request.GET.get('error')
-    url = 'https://accounts.spotfy.com/api/token'
-    response = request.post(url=url,data={
+    url = 'https://accounts.spotify.com/api/token'
+    response = requests.post(url=url,data={
         'grant_type' : 'authorization_code',
         'code' : code,
         'redirect_uri' : REDIRECT_URI,
@@ -38,8 +39,8 @@ def spotify_callback(request, format=None):
     expires_in = response.get('expires_in')
     error = response.get('error')
 
-    if not self.request.session.exists(self.request.session.session_key):
-        self.request.session.create()
+    if not request.session.exists(request.session.session_key):
+        request.session.create()
     
     update_or_create_tokens(session_key=request.session.session_key, access_token=access_token, refresh_token=refresh_token, token_type=token_type, expires_in=expires_in)
 
@@ -49,3 +50,45 @@ class IsAuthenticated(APIView):
     def get(self, request, format=None):
         is_authenticated = is_spotify_authenticated(self.request.session.session_key)
         return Response({'status': is_authenticated},status=status.HTTP_200_OK)
+
+class CurrentSong(APIView):
+    def get(self, request, format=None):
+        room_code = self.request.session.get('room_code')
+        room_query = Room.objects.filter(code=room_code)
+        if room_query:
+            room = room_query[0]
+            host = room.host
+            endpoint = "player/currently-playing"
+            response = make_spotify_api_request(host,endpoint,'GET')
+            
+            if 'error' in response or 'item' not in response:
+                return Response({}, status=status.HTTP_204_NO_CONTENT)
+
+            item = response.get('item')
+            duration = item.get('duration_ms')
+            progress = response .get('progress_ms')
+            album_cover = item.get('album').get('images')[0].get('url')
+            is_playing = response.get('is_playing')
+            song_id = item.get('id')
+            title = item.get('name')
+            artist_string = ""
+
+            for i, artist in enumerate(item.get('artists')):
+                if i > 0:
+                    artist_string += ", "
+                name = artist.get('name')
+                artist_string += name
+
+            song = {
+                'title': title,
+                'artist': artist_string,
+                'duration' : duration,
+                'time' : progress,
+                'image_url' : album_cover,
+                'is_playing' : is_playing,
+                'votes':0,
+                'id': song_id
+            }
+            return Response(song, status=status.HTTP_200_OK)
+        return Response({'Msg':'No Room Found'}, status=status.HTTP_404_NOT_FOUND)
+        
